a4486c71eb8128fcf3e917a46c3c218973fbbcd1
dhurtado9611 <50833469+dhurtado9611@users.noreply.github.com>
2024-10-17 23:45:13 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+let timerId = setTimeout(function(){
+    $('#load').fadeOut();
+    $('body').removeClass('hidden');
+    /* alert('Pagina WEB protegida con AvastAntivirus'); */
+  }, 2000);
+
+/* window.onload = function(){
+    $('#load').fadeOut();
+} */
